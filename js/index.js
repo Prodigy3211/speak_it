8420f7a206214d951aceda7decce2835ce6a66c2
Prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2024-05-12 00:00:11 -0400

    Update some HTML and js
    I am working on getting the form into tip top shape
    so that when you fill out the data it successfully send it to the serve
    r so that it can better be stored and tested.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
-alert("hey")
-document.body.innerHTML =  "Hello";
+// alert("hey")
+// document.body.innerHTML =  "Hello";
+
+// const inputs = document.getElementsByTagName('input');
+
+// console.log(inputs);
+
+const submitForm = document.querySelector('button');
+
+console.log(submitForm)
 
 
