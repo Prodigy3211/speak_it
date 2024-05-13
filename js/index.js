a139d6e941c244098a64b2ec6d861bafa42e847c
Prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2024-05-13 13:53:25 -0400

    Add new pages where information is stored
    Needed a place for new threads to appear after user creates one.
    
    After I make the CSS for this. I plan to make the forms interactive.

--- js/index.js
+++ js/index.js
@@ -9,4 +9,12 @@ const submitForm = document.querySelector('button');
 
 console.log(submitForm)
 
+// Import Form to DOM
+const newPost = document.querySelectorAll('newThreadForm p);
 
+newPost.addEventListener('submit', function(e){
+    alert('Hello world');
+    console.log(e.target);
+    console.log(e);
+});
+ 
