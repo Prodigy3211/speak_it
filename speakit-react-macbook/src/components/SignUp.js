1493d5b0d76abac6dd7474e84a6c288f900ff48a
prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2024-08-30 22:44:22 -0400

    Add functionality for signUp page
    The signup page will now insert user data into the database.

---
+++ speakit-react-macbook/src/components/SignUp.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function SignUp(){
+    const {username, setUsername} = useState('');
+    const {email, setEmail} = useState('');
+    const {password, setPassword} = useState('');
+
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('https://localhost:5000/signup',{
+                username,
+                email,
+                password,
+            });
+            
+            alert(response.data.message);
+        } catch (error){
+            console.error(error);
+            alert('Error Signing Up');
+        }
+    };
+    
+    return (
+        <form onSubmit={handleSignUp}>
+            <input 
+                type ='text'
+                placeholder='Username'
+                value = {username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+            />
+            <input
+                type='email'
+                placeholder='Email Address'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+            />
+            <input
+                type='text'
+                placeholder='Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+            />
+            <button type='submit'>Create Account</button>
+        </form>
+    );
+}
+
+export default SignUp;
+
