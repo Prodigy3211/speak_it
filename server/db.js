03d1e3dae6175d3e9642cf73ed64032b24b9e1f2
prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2024-09-01 00:50:48 -0400

    Update react and server files
    Now that I can connect to the backend server using express. I need to figure out
    why the React app isn't rendering. It appears to be some dependencies that need to
    be resolved.

--- server/db.js
+++ server/db.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host:"",
-    user:,
-    password:,
-    database:,
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
 });
 
 connection.connect((err) => {
--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const  express = require('express');
+const  express = require ("express");
 const bcrypt = require ('bcryptjs');
 const jwt = require('jsonwebtoken');
 const connection = require('./db');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 //middleware
 
