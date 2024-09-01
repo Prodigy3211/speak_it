951b9f4dda3890169fe155162ae5a24e1996c421
prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2024-08-31 22:09:37 -0400

    Add Login Functionality to Express server
    Now that the signup is set up, it was time for login. Will start testing now.

---
+++ speakit-react-macbook/src/components/Login.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import axios from 'axios';
+
+function Login () {
+    const [email , setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    cost handleLogin = async (e) => {
+        e.preventDefault();
+        
+        try {
+            const response = await axios.post('https://localhost:5000/login', {
+                email,
+                password,
+            });
+
+            alert(response.data.message);
+            localStorage.setItem('token',response.data.token); //saves data for later
+        } catch (error){
+            console.error(error);
+            alert('Error Logging In :(');
+        }
+    };
+
+    return (
+        <form onSubmit={handleLogin}>
+            <input
+            type ="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            />
+            <input
+            type ="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            />
+            <button type="submit">Login</button>
+        </form>
+    );
+}
+
+
+export default Login;
