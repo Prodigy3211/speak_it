df386ee98ca074b6aa2209ec479f480c28410b41
prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2024-09-16 00:30:10 -0400

    Add new Update profile information function
    Needed a way to update the the put request now allows a logged in user
    to update thier bio.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const  express = require ("express");
-const cors = require ('cors');
-const bcrypt = require ('bcryptjs');
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const connection = require('./db');
 
@@ -16,90 +16,139 @@ app.use(cors());
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
+//Token authentification
+
+const authenticateToken = (req, res, next) => {
+  //Read Authorization Header
+  const authHeader = req.headers['authorization'];
+  //Get token from Bearer Token in the Frontend
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'No Token Provided' });
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid Token' });
+    }
+    //attach user with id
+    req.user = user;
+    next();
+  });
+};
 
 //homepage route
 
-app.get ('/', async (req,res) => {
-    res.send('Welcome to Speakit');
+app.get('/', async (req, res) => {
+  res.send('Welcome to Speakit');
 });
 
 //Sign Up Page Route
 
 app.post('/signup', async (req, res) => {
-    const {username, email, password} = req.body;
+  const { username, email, password } = req.body;
 
-    //Hashout the Password
-    const hashedPassword = await bcrypt.hash(password, 10);
+  //Hashout the Password
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    //Insert new User into the Database
-    connection.query(
-        'INSERT INTO users (username, email, password) VALUES (? , ? , ?)',
-        [username, email, hashedPassword],
-        (err, result) => {
-            if (err){
-                return res.status(500).json({message:'Error registering user'});
-            }
-            res.status(201).json({message:'User successfully registered!'});
-        }
-    );
+  //Insert new User into the Database
+  connection.query(
+    'INSERT INTO users (username, email, password) VALUES (? , ? , ?)',
+    [username, email, hashedPassword],
+    (err, result) => {
+      if (err) {
+        return res.status(500).json({ message: 'Error registering user' });
+      }
+      res.status(201).json({ message: 'User successfully registered!' });
+    }
+  );
 });
 
 //Login Route
 
-app.post('/login' , (req, res) => {
-    const {email, password} = req.body;
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
 
-    //Does User Exist?
-    connection.query(
-        'SELECT * FROM users WHERE email = ?',
-        [email],
+  //Does User Exist?
+  connection.query(
+    'SELECT * FROM users WHERE email = ?',
+    [email],
     async (err, results) => {
-        if(err){
-            return res.status(500).json({message:'Error logging in'});
-        }
-        if (results.length === 0) {
-            return res.status(400).json({message:'User Not Found...'});
-        }
+      if (err) {
+        return res.status(500).json({ message: 'Error logging in' });
+      }
+      if (results.length === 0) {
+        return res.status(400).json({ message: 'User Not Found...' });
+      }
 
-        const user = results[0];
+      const user = results[0];
 
-        //Password Check
-        const isMatch = await bcrypt.compare(password,user.password);
-        if (!isMatch){
-            return res.status(400).json({message:'Invalid Credentials'});
-        }
+      //Password Check
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: 'Invalid Credentials' });
+      }
 
-        //Create JWT Token
-        const token = jwt.sign({id:user.id}, JWT_SECRET, {
-            expiresIn:'1h',
-        });
-        res.json({message:'Login Successful!', token});
+      //Create JWT Token
+      const token = jwt.sign({ id: user.id }, JWT_SECRET, {
+        expiresIn: '1h',
+      });
+      res.json({ message: 'Login Successful!', token });
     }
-    );
+  );
 });
 
 //My Profile Route
 
-app.get('/my-profile' , (req,res) => {
-    const token = req.headers.authorization.split(' ')[1];
-        try {
-            const decoded = jwt.verify(token , JWT_SECRET);
-            const userId = decoded.id;
-        //Fetch userID data from the database
-            connection.query('SELECT username, email FROM users WHERE id = ?',
-            [userId], (err, results) => {
-                if (err) {
-                    return res.status(500).json ({ mesage: 'Error fetching user data'});
-                }
-                    const user = results[0];
-                    res.json(user);
-            });
-        } catch (error) {
-            res.status(401).json({message:'Unauthorized access'});
+app.get('/my-profile', (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    const userId = decoded.id;
+    console.log(userId);
+    //Fetch userID data from the database
+    connection.query(
+      'SELECT username, email FROM users WHERE id = ?',
+      [userId],
+      (err, results) => {
+        if (err) {
+          return res.status(500).json({ mesage: 'Error fetching user data' });
         }
+        const user = results[0];
+        res.json(user);
+      }
+    );
+  } catch (error) {
+    res.status(401).json({ message: 'Unauthorized access' });
+  }
+});
+
+//edit profile request
+
+app.put('/my-profile', authenticateToken, (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+  const { bio } = req.body;
+  //Get Id from token auth
+  const userId = req.user.id;
+
+  if (!userId) {
+    return res.status(400).json({ message: 'User ID is undefined' });
+  }
+
+  connection.query(
+    'UPDATE users SET bio = ? WHERE id = ?',
+    [bio, userId],
+    (err, result) => {
+      if (err) {
+        return res.status(500).json({ message: 'Error updating Bio' });
+      }
+      res.status(200).json({ message: 'Bio Updated Successfully!' });
+    }
+  );
 });
 
 //start Server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
--- speakit-react-macbook/src/components/Profile.js
+++ speakit-react-macbook/src/components/Profile.js
@@ -3,51 +3,94 @@ import axios from 'axios';
 import LogoutButton from './LogoutButton';
 
 function Profile() {
-    const [userData, setUserData ] = useState(null);
-        
-        useEffect(() => {
-            //Fetch the user data after login
-            const fetchUserData = async () => {
-                const token = localStorage.getItem('token');
-                    if (!token) {
-                        console.error('No Token Found. Please Log in again.');
-                    }
-                const config = {
-                    headers:
-                    {Authorization : `Bearer ${token}`}
-                };
-                try {
-                    const response = await axios.get(
-                        'http://localhost:8000/my-profile', config);
-                    
-                    setUserData(response.data);
-                } catch (error) {
-                    console.error(error);
-                }
-            };
-            fetchUserData();
-        }, []);
-    
-    return(
-        <div>
-            <h1>My Profile Page</h1>
-            <p> I hate my life</p>
-            {userData ?(
-            <div>
-                <p>
-               Username: {userData.username}
-            </p>
-            <p>
-                Email: {userData.email}
-            </p>
-            </div>
-            ) : (
-                <p>Loading...</p>
-            )}
+  const [userData, setUserData] = useState(null);
+  const [bio, setBio] = useState('');
 
-            <LogoutButton />
+  useEffect(() => {
+    //Fetch the user data after login
+    const fetchUserData = async () => {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        console.error('No Token Found. Please Log in again.');
+      }
+      const config = {
+        headers: { Authorization: `Bearer ${token}` },
+      };
+      try {
+        const response = await axios.get(
+          'http://localhost:8000/my-profile',
+          config
+        );
+
+        setUserData(response.data);
+        setBio(response.data.bio);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchUserData();
+  }, []);
+
+  const handleBioChange = (e) => {
+    setBio(e.target.value);
+  };
+
+  const handleProfileUpdate = async (e) => {
+    e.preventDefault();
+
+    const token = localStorage.getItem('token');
+    console.log(token);
+
+    try {
+      const response = await axios.put(
+        'http://localhost:8000/my-profile',
+        {
+          bio,
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      //Update User Data with new information
+      setUserData({ ...userData, bio });
+      alert(response.data.message);
+    } catch (error) {
+      console.log('error updating profile', error);
+      alert('error updating profile');
+    }
+  };
+
+  return (
+    <div>
+      <h1>My Profile Page</h1>
+      <p> I hate my life</p>
+      {userData ? (
+        <div>
+          <p>Username: {userData.username}</p>
+          <p>Email: {userData.email}</p>
+          <p>Bio {userData.bio}</p>
+
+          <h2>Edit Bio</h2>
+          <form onSubmit={handleProfileUpdate}>
+            <div>
+              <label>Bio:</label>
+              <textarea
+                value={bio}
+                onChange={handleBioChange}
+                placeholder='Tell us about yourself..'
+              />
+            </div>
+            <button type='submit'> Save changes</button>
+          </form>
         </div>
-    );
+      ) : (
+        <p>Loading...</p>
+      )}
+
+      <LogoutButton />
+    </div>
+  );
 }
 
 export default Profile;
