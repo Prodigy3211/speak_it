39d169393245ccfa1c8762a00e5e373e24e7d60e
prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2024-09-28 13:24:39 -0400

    Add supabase information
    We are migrating to supabase!

--- speakit-react-macbook/src/components/Login.js
+++ speakit-react-macbook/src/components/Login.js
@@ -1,57 +1,57 @@
-import { useState } from "react";
+import { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import supabase from '../server/supabaseClient';
 
 //Allows a user to enter their username and password
 //If successful, will redirect to "My Profile page"
+console.log(supabase);
 
-function Login () {
-    const [email , setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
-    
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        
-        try {
-            const response = await axios.post('http://localhost:8000/login', {
-                email,
-                password,
-            });
-            if (response.data.message === 'Login Successful!')
-                {
-                    alert(response.data.message);
-                    //saves data for later
-                    localStorage.setItem('token',response.data.token);
-                    //Send to My Profile page.
-                    navigate('/my-profile');
-            }
-        } catch (error){
-            console.error(error);
-            alert('Error Logging In :(');
-        }
-    };
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-    return (
-        <form onSubmit={handleLogin}>
-            <input
-            type ="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            />
-            <input
-            type ="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            />
-            <button type="submit">Login</button>
-        </form>
-    );
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:8000/login', {
+        email,
+        password,
+      });
+      if (response.data.message === 'Login Successful!') {
+        alert(response.data.message);
+        //saves data for later
+        localStorage.setItem('token', response.data.token);
+        //Send to My Profile page.
+        navigate('/my-profile');
+      }
+    } catch (error) {
+      console.error(error);
+      alert('Error Logging In :(');
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <input
+        type='email'
+        placeholder='Email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input
+        type='password'
+        placeholder='Password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <button type='submit'>Login</button>
+    </form>
+  );
 }
-
 
 export default Login;
