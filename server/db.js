dc0b34b922e99535a97d5c328a0330143cfbb376
prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2024-11-26 20:22:34 -0500

    Remove old database set up
    Now removing local database set up.

--- server/db.js
+++ server/db.js
@@ -1,20 +1,20 @@
 
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const env = require ('dotenv').config();
+// const env = require ('dotenv').config();
 
 
 
-const connection = mysql.createConnection({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME,
-});
+// const connection = mysql.createConnection({
+//     host:process.env.DB_HOST,
+//     user:process.env.DB_USER,
+//     password:process.env.DB_PASSWORD,
+//     database:process.env.DB_NAME,
+// });
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log ('Connected to MySql Database');
-});
+// connection.connect((err) => {
+//     if (err) throw err;
+//     console.log ('Connected to MySql Database');
+// });
 
-module.exports = connection;
+// module.exports = connection;
