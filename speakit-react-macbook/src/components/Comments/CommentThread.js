580e2c2a84e1d7305582f3dfe2c25ce8462655a7
prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2025-05-30 14:15:08 -0400

    Working on new reply hiding feature

--- speakit-react-macbook/src/components/Comments/CommentThread.js
+++ speakit-react-macbook/src/components/Comments/CommentThread.js
@@ -1,9 +1,12 @@
-import {useRef, useEffect} from 'react';
+import {useRef, useEffect, useState} from 'react';
 import CommentItem from './CommentItem';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const CommentThread = ({comments, onCommentAdded}) => {
     const commentRefs = useRef({});
+    const[expandedThreads, setExpandedThreads] = useState({});
 
     //create a ref for each comment
     useEffect(() => {
@@ -32,11 +35,30 @@ const CommentThread = ({comments, onCommentAdded}) => {
         return acc;
     }, {});
 
+    //Function to toggle the thread expansion
+    const toggleThread = (comment_id) => {
+        setExpandedThreads(prev => ({
+            ...prev,
+            [comment_id]: !prev[comment_id]
+        }));
+    };
+
     // render comments recursively
     const renderCommentGroup = (parentId, level = 0) => {
         const groupComments = commentGroups[parentId] || [];
+        const isExpanded = expandedThreads[parentId] || false;
+        const shouldCollapse = groupComments.length >= 1 && !isExpanded;
 
-        return groupComments.map(comment => (
+        const visibleComments = shouldCollapse ? groupComments.slice(0, 3) : groupComments;
+
+    console.log('Parent ID:', parentId);
+    console.log('Number of comments:', groupComments.length);
+    console.log('Is expanded:', isExpanded);
+    console.log('Should collapse:', shouldCollapse);
+
+        return (
+        <>
+        {visibleComments.map(comment => (
             <div 
                 key={comment.id}
                 ref={commentRefs.current[comment.id]}
@@ -53,8 +75,19 @@ const CommentThread = ({comments, onCommentAdded}) => {
                     </div>
                 )}
             </div>
-        ));
-    };
+        ))}
+        {shouldCollapse && (
+            <button
+                onClick={() => toggleThread(parentId)}
+                className="flex items-center gap-1 text-sm text-red-500 hover:text-gray-700 border-2 border-solid border-red-500 rounded-md p-1"
+            >
+                <FontAwesomeIcon icon={isExpanded ? faChevronUp : faChevronDown} className="mr-2" />
+                {isExpanded ? 'Hide' : 'Show'} {groupComments.length - 1} replies
+            </button>
+        )}
+        </>
+    );
+};
 
     return (
         <div>
@@ -62,5 +95,6 @@ const CommentThread = ({comments, onCommentAdded}) => {
         </div>
     );
 };
+
 
 export default CommentThread;
