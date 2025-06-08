eedda4e9d7fc553da7e28499ec5f5e4edf20899c
prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2025-06-08 12:15:36 -0400

    Update meta tags for sharing

--- speakit-react-macbook/src/components/ShareButton.js
+++ speakit-react-macbook/src/components/ShareButton.js
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShare } from '@fortawesome/free-solid-svg-icons';
 import { useState, useEffect } from 'react';
 import supabase from '../server/supabaseClient';
+import { Helmet } from 'react-helmet';
 
 const ShareButton = ({ claimId }) => {
     const [showCopied, setShowCopied] = useState(false);
@@ -13,7 +14,7 @@ const ShareButton = ({ claimId }) => {
             
             const { data, error } = await supabase
                 .from('claims')
-                .select('title')
+                .select('title, content')
                 .eq('id', claimId)
                 .single();
             
@@ -65,6 +66,28 @@ const ShareButton = ({ claimId }) => {
     };
 
     return (
+        <>
+        <Helmet>
+            {/* Meta tags for Sharing*/}
+            <title>{claim ? `${claim.title} - Do you agree or disagree?` : 'Check out this claim'}</title>
+            <meta name="description" content = {claim ? `${claim.title} - Join the discussion on Speak It!` : 'Check out this claim'} />
+
+            {/* Open Graph tags for Sharing*/}
+            <meta property="og:title" content = {claim ? `${claim.title} - Do you agree?` : 'Check out this Hot Take'} />
+            <meta property="og:description" content = {claim?.content || 'Join the discussion!'} />
+            <meta property="og:url" content= {window.location.href} />
+            <meta property = "og:type" content="website" />
+
+            {/* Twitter Card tags for Sharing*/}
+            <meta name="twitter:card" content="summary" />
+            <meta name = "twitter:title" content = {claim ? `${claim.title} - Do you agree or disagree?` : 'Check out this Hot Take!'} />
+            <meta name = "twitter.description" content = {claim?.content || 'Join the discussion!'} />
+
+            {/* Defaul Image */}
+            <meta property="og:image" content= "https://qdpammoeepwgapqyfrrh.supabase.co/storage/v1/object/public/speak-it-brand-assets//speak-itHeader.png"/>
+            <meta name="twitter:image" content= "https://qdpammoeepwgapqyfrrh.supabase.co/storage/v1/object/public/speak-it-brand-assets//speak-itHeader.png"/>
+
+        </Helmet>
         <button 
             className="bg-blue-500 text-white px-4 py-2 rounded-md flex items-center gap-2 hover:bg-blue-600 transition-colors"
             onClick={handleShare}
@@ -72,6 +95,7 @@ const ShareButton = ({ claimId }) => {
             <FontAwesomeIcon icon={faShare} />
             {showCopied ? 'Copied!' : 'Share Claim'}
         </button>
+        </>
     );
 };
 
